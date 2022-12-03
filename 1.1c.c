382a71c603638f0edf239677da667ec28b330544
VAGHASIYA PRATIK <118496001+PRATIKVAGHASIYA8124@users.noreply.github.com>
2022-12-03 14:11:22 +0530

    SIMPLE TYPE PROGRAM

---
+++ 1.1c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    printf("%C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C\n",1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 );
+    printf("%C \"if you are resisting something, you are feeding it.                  %C\n" ,4 ,4);
+    printf("%C      Any energy you fight, you are feeding it.                        %C\n" ,3 ,3);
+    printf("%C              If you are pushing something away,                       %c\n" ,1 ,1);
+    printf("%C                      you are inviting it to stay. \"by Michael Singer  %C\n" ,4 ,4);
+     printf("%C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C %C\n",1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 ,4 ,3 ,1 );
+}
